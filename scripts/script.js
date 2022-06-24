7a59fc8ab65e5da5b92b096181d3f63bb939a59c
oleGAPrelev <101264107+oleGAPrelev@users.noreply.github.com>
2022-06-24 12:17:31 +0200

    form started, array collects data

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,16 @@
+const fotmElem = document.querySelector('.workers_form');
+let workersArr = [];
 
+fotmElem.addEventListener('submit', (evnt) => {
+	event.preventDefault();
+	const { fristname, lastname, age } = event.target; //this
+	workersArr.push({
+		fristname: fristname.value,
+		lastname: lastname.value,
+		age: age.value,
+	});
+	fristname.value = '';
+	lastname.value = '';
+	age.value = '';
+	console.log(workersArr);
+});
